cf56c448ce3f4c874e29d9161cefcf9438a08ed4
Seif Omar Awean <seifomaraly123@gmail.com>
2026-05-04 17:44:22 +0300

    Update next.config.mjs

--- hotel-frontend/next.config.mjs
+++ hotel-frontend/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Enable static export for images
   images: {
     unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    output: "export", // Enable static export for images
+    
   },
 };
 
